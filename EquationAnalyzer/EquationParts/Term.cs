c638964de41fd5df035dcb9c455189895375c57b
Arne Kiesewetter <banane1996@gmx.net>
2014-04-03 14:51:02 +1300

    Basic Stuff.

---
+++ EquationAnalyzer/EquationParts/Term.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationAnalyzer.EquationParts
+{
+    public class Term
+    {
+        public IOperation Operation { get; set; }
+
+        /// <summary>
+        /// Get the value of the Term.
+        /// </summary>
+        /// <returns>The value of the Term.</returns>
+        public double GetValue(Dictionary<string, double> variables)
+        {
+            return Operation.GetValue(variables);
+        }
+    }
+}
